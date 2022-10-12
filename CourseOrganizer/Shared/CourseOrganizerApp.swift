2c1a4e28739a0d62daba434a8908cf5cfe30349e
Giang Nguyen <gn02.nguyen@gmail.com>
2022-10-12 12:42:42 -0400

    Create Swift project

---
+++ CourseOrganizer/Shared/CourseOrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseOrganizerApp.swift
+//  Shared
+//
+//  Created by Giang Nguyen on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CourseOrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
