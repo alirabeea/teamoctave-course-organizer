e45328811cbe65e85134c850396f9de38d6cfd5e
Sally Rademacher <srademacher@umass.edu>
2022-10-24 18:51:43 -0400

    User info page

--- CourseOrganizer_UI/CourseOrganizer_UI/Views/UserView.swift
+++ CourseOrganizer_UI/CourseOrganizer_UI/Views/UserView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct UserView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("User Info")
+                .font(.largeTitle)
+            Spacer()
+        }
     }
 }
 
