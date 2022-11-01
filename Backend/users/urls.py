5e71a5bdefeaa8ee4f3bdbab8b672757f4280a82
Mohit Ganna <mohitganna@gmail.com>
2022-11-01 14:30:15 -0400

    Added ability to register new users

--- Backend/users/urls.py
+++ Backend/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import LoginView, LogoutView
+from .views import LoginView, LogoutView, RegisterView
 
 urlpatterns = [
-    path('login/', LoginView.as_view()),
-    path('logout/', LogoutView.as_view())
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register')
 ]
--- Backend/users/views.py
+++ Backend/users/views.py
@@ -1,8 +1,9 @@
 from django.views import View
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
 from django.http import JsonResponse
-from .models import User
+from .forms import UserForm
 
 class LoginView(View):
     def get(self, request, *args, **kwargs):
@@ -15,6 +16,22 @@ class LoginView(View):
             return JsonResponse({"status": "Success!", "status_code": 201})
         else:
             return JsonResponse({"status": "Error!", "status_code": 401})
+
+class RegisterView(View):
+    django_user_form = UserCreationForm()
+    user_form = UserForm()
+
+    def get(self, request, *args, **kwargs):
+        return JsonResponse({"fields": ("name", "netid", "username", "password1", "password2")})
+
+    def post(self, request, *args, **kwargs):
+        django_user = UserCreationForm(request.POST)
+        user = UserForm(request.POST)
+        if django_user.is_valid() and user.is_valid():
+            django_user.save()
+            user.save()
+            return JsonResponse({"status": "Success!", "status_code": 201})
+        return JsonResponse({"status": "Error!", "status_code": 401} + user.errors + django_user.errors)
 
 class LogoutView(LoginRequiredMixin, View):
     def get(self,request, *args, **kwargs):
