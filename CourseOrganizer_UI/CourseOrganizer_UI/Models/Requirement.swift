a4d5c4982de566468a459104d3a59a82a7e9058e
Sally Rademacher <srademacher@umass.edu>
2022-11-02 19:11:47 -0400

    Created Requirement model

---
+++ CourseOrganizer_UI/CourseOrganizer_UI/Models/Requirement.swift
@@ -0,0 +1,12 @@
+//
+//  Requirement.swift
+//  CourseOrganizer_UI
+//
+//  Created by Sally Rademacher on 11/2/22.
+//
+
+import Foundation
+
+struct Requirement: Codable {
+    //figure out vars for requirements
+}
