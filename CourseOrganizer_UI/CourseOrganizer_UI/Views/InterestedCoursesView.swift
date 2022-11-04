615a5a4f1b1c618e1ebdb7bbaf7532751148ac26
Sally Rademacher <srademacher@umass.edu>
2022-11-04 15:49:52 -0400

    Added "add a course" text field to InterestedCoursesView

--- CourseOrganizer_UI/CourseOrganizer_UI/Views/InterestedCoursesView.swift
+++ CourseOrganizer_UI/CourseOrganizer_UI/Views/InterestedCoursesView.swift
@@ -8,15 +8,35 @@
 import SwiftUI
 
 struct InterestedCoursesView: View {
+    @State private var newCourse = ""
     var body: some View {
         VStack{
             Text("Interested Courses")
                 .font(.largeTitle)
             Spacer()
             
+            Text("Add a course")
+            TextField("Enter course name", text: $newCourse)
+                .padding()
+                .frame(width: 300, height: 50)
+                .background(Color.black.opacity(0.05))
+                .cornerRadius(10)
+                .onSubmit{
+                    addInterestedCourse(course: newCourse)
+                }
+            
+            Spacer()
+            
             
         }
     }
+}
+func addInterestedCourse(course: String){
+    
+}
+
+func getInterestedCourses(){
+    
 }
 
 struct InterestedCoursesView_Previews: PreviewProvider {
