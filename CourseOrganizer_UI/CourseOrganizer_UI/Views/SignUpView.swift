52163eae923b8b1eb272f60f21100658a71e672c
Sally Rademacher <srademacher@umass.edu>
2022-11-06 14:44:56 -0500

    working on signup api connection

--- CourseOrganizer_UI/CourseOrganizer_UI/Views/SignUpView.swift
+++ CourseOrganizer_UI/CourseOrganizer_UI/Views/SignUpView.swift
@@ -10,8 +10,10 @@ import SwiftUI
 struct SignUpView: View {
     
     @StateObject private var userVM = UserViewModel()
+    @State var message: String = "";
     var body: some View {
         VStack{
+           
             Text("Sign Up")
                 .font(.largeTitle)
             
@@ -42,67 +44,90 @@ struct SignUpView: View {
                 .background(Color.black.opacity(0.05))
                 .cornerRadius(10)
             Button("Create account"){
-                createAccount(firstName: userVM.userInfo.firstName, netid: userVM.userInfo.netid, email: userVM.userInfo.email, username: userVM.userInfo.username, password: userVM.userInfo.password)
-                
+                getCSRF()
+//                createAccount(firstName: userVM.userInfo.firstName, netid: userVM.userInfo.netid, email: userVM.userInfo.email, username: userVM.userInfo.username, password: userVM.userInfo.password)
+//
             }.foregroundColor(.white)
                 .frame(width: 300, height: 50)
                 .background(Color.blue)
                 .cornerRadius(10)
-                .disabled(userVM.createUserDisabled)
+                //.disabled(userVM.createUserDisabled)
+            Text(message)
         }.autocapitalization(.none)
         
         Spacer()
     }
-   
-}
-    
-struct Message: Decodable {
-        let data: String;
-    }
-    
-    
-    //connect to backend, add new user with info
-    //entered user info is stored in userVM.userInfo
-    //as .firstName, .email, .password
-func createAccount(firstName: String, netid: String, email: String, username: String, password: String) {
+    struct Message: Decodable {
+            let data: String;
+        }
+        
+        
+    func getCSRF(){
         guard let url = URL(string: "http://127.0.0.1:8000/users/register/") else {
             print("api is down")
             return
         }
-        
         var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        let json = [
-            "firstname": firstName,
-            "netid": netid,
-            "username": username,
-            "password1": password,
-            "password2": password]
-
+        request.httpMethod = "GET"
         
         URLSession.shared.dataTask(with: request) { data, response, error in
             if let data = data {
                 if let response: Message = try? JSONDecoder().decode(Message.self, from: data) {
                     DispatchQueue.main.async {
-                        
-                            do {                                    request.httpBody = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted) // pass dictionary to data object and set it as request body
-                                } catch let error {
-                                    print(error.localizedDescription)
-                                }
-            
-                        print(response.data);
+                        message = response.data
+                        print("hi")
+                        print(response.data)
                     }
                     return
-                }
-                if let response = response as? HTTPURLResponse {
-                    if(response.statusCode == 201){
-                        ContentView()
-                    }
                 }
             }
             
         }.resume()
+        return
     }
+    func createAccount(firstName: String, netid: String, email: String, username: String, password: String) {
+            guard let url = URL(string: "http://127.0.0.1:8000/users/register/") else {
+                print("api is down")
+                return
+            }
+            
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            let json = [
+                "firstname": firstName,
+                "netid": netid,
+                "username": username,
+                "password1": password,
+                "password2": password]
+
+            
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                if let data = data {
+                    if let response: Message = try? JSONDecoder().decode(Message.self, from: data) {
+                        DispatchQueue.main.async {
+                            
+                                do {                                    request.httpBody = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted) // pass dictionary to data object and set it as request body
+                                    } catch let error {
+                                        print(error.localizedDescription)
+                                    }
+                
+                            print(response.data);
+                        }
+                        return
+                    }
+                    if let response = response as? HTTPURLResponse {
+                        if(response.statusCode == 201){
+                            ContentView()
+                        }
+                    }
+                }
+                
+            }.resume()
+        }
+   
+}
+    
+
 
 
 struct SignUpView_Previews: PreviewProvider {
