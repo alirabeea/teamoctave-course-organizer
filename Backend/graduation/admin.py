98ab8528860ab98c776f34880a8332b93b760d9a
Mohit Ganna <mohitganna@gmail.com>
2022-11-08 03:02:35 -0500

    Registered admin models

--- Backend/graduation/admin.py
+++ Backend/graduation/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Major, Requirement, Requirement_Course
 
 # Register your models here.
+admin.site.register(Major)
+admin.site.register(Requirement)
+admin.site.register(Requirement_Course)
