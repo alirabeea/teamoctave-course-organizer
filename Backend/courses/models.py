7fd366ad91f752d39310d28497c6ddf3da501fdf
Mohit Ganna <mohitganna@gmail.com>
2022-11-30 13:06:41 -0500

    Wrote and run script to add courses and reset db

--- Backend/courses/models.py
+++ Backend/courses/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 class Course(models.Model):
     number = models.CharField(max_length=64)
+    name = models.CharField(max_length=64)
+    department = models.CharField(max_length=64)
+    credits = models.IntegerField()
 
     def __str__(self):
         return f"{self.number}"
